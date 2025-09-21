3d0c30976c14abc6860bf9bc0c0cc0909b6f0518
Kunal-Paste <pastekunal6@gamail.com>
2025-09-21 19:08:09 +0530

    Add initial HTML and SVG files for Vite + React setup
    - Created index.html with basic structure and linked assets
    - Added vite.svg for the favicon

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -18,7 +18,7 @@ const Register = () => {
         setSubmitting(true);
         console.log(form);
 
-        axios.post("http://localhost:3000/api/auth/register", {
+        axios.post("https://cosmicai-01zg.onrender.com/api/auth/register", {
             email: form.email,
             fullname: {
                 firstname: form.firstname,
